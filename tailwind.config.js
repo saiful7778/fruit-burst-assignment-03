d6f28d44149484097b8f86518f8edc5208c69ebb
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-07-25 23:08:50 +0600

    add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1496px",
+      },
+    },
+    extend: {
+      colors: {
+        "clr-pri": "#F85559",
+        "dark-01": "#121212",
+        "dark-02": "#12121299",
+      },
+    },
+    fontFamily: {
+      loto: ["Lato", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
